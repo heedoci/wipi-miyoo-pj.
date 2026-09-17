71f7ec120c97e8bf14c3b62f991420e88834aa07
heedoci <gksehdgml00@gmail.com>
2026-09-18 00:34:13 +0900

    Exercise exit combo shim in smoke test

--- test/smoke_test.sh
+++ test/smoke_test.sh
@@ -6,6 +6,7 @@ DUMMY="${TMPDIR:-/tmp}/wipi-miyoo-dummy.zip"
 printf dummy > "$DUMMY"
 cc -std=c11 -Wall -Wextra -Werror \
   -I"$ROOT/test/fake_sdl" -I"$ROOT/include" \
+  -include "$ROOT/include/miyoo_exit_combo.h" \
   "$ROOT/src/wipi_miyoo.c" "$ROOT/test/fake_sdl.c" "$ROOT/test/fake_wipi.c" \
   -o "$OUT"
 "$OUT" "$DUMMY" "${TMPDIR:-/tmp}/wipi-miyoo-save"
